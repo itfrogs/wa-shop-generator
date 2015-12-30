e2f93eac0b8fce590fbbe799f3b2493794d3bcdc
Andrew Godin <covoxx@gmail.com>
2015-12-30 03:00:27 +0300

    полная очистка run контроллера

--- lib/actions/shopGeneratorPluginRun.controller.php
+++ lib/actions/shopGeneratorPluginRun.controller.php
@@ -2,8 +2,6 @@
 
 class shopGeneratorPluginRunController extends waLongActionController
 {
-    /** @var shopGeneratorPluginReader */
-    private $reader;
 
     public function execute()
     {
@@ -35,37 +33,15 @@ class shopGeneratorPluginRunController extends waLongActionController
             $options = waRequest::post();
             $options['processId'] = $this->processId;
 
-
-
-            if ($reader_id = waRequest::post('reader')) {
-                unset($options['reader']);
-            }
-
-            //todo: я тут ридер удалил, его надо посмотреть в основном плагине т.к. на него все завязано
-
-            $this->data['reader'] = $this->getReader($reader_id, $options['config']);
-
-            $this->reader = $this->data['reader'];
-            $this->reader->init();
-
             $this->data += array(
                 'timestamp'  => time(),
-                'count'      => $this->reader->count(),
+                'count'      => $options['config']['num'],
+                'current'    => $options['config']['num'],
+                'processed_count'    => 0,
                 'error'      => NULL,
                 'memory'     => memory_get_peak_usage(),
                 'memory_avg' => memory_get_usage(),
             );
-
-            if(isset($options['config']['clear_qty']) && isset($options['config']['clear_qty'])) {
-                $this->data['count']['clear_qty'] = 1;
-            }
-
-            $stages = array_keys($this->data['count']);
-            $this->data['current'] = array_fill_keys($stages,0);
-            $this->data['processed_count'] = array_fill_keys($stages,0);
-            $this->data['stage'] = reset($stages);
-            $this->data['stage_name'] = $this->getStageName($this->data['stage']);
-
 
         } catch (waException $e) {
             echo json_encode(array('error' => $e->getMessage()));
@@ -86,9 +62,11 @@ class shopGeneratorPluginRunController extends waLongActionController
      */
     protected function step()
     {
-        $step = $this->reader->step($this->data['current'], $this->data['count'], $this->data['processed_count'], $this->data['stage'], $this->data['error']);
+        $step = 0;
         $this->data['memory'] = memory_get_peak_usage();
         $this->data['memory_avg'] = memory_get_usage();
+        $this->data['current']--;
+        $this->data['processed_count'] = $this->data['count'] - $this->data['current'];
         return !$this->isDone() && $step;
     }
 
@@ -103,16 +81,6 @@ class shopGeneratorPluginRunController extends waLongActionController
     protected function isDone()
     {
         $done = true;
-        foreach ($this->data['current'] as $stage => $current) {
-            if ($current < $this->data['count'][$stage]) {
-                $done = false;
-                $this->data['stage'] = $stage;
-                break;
-            }
-        }
-        if ($this->reader) {
-            $this->data['stage_name'] = $this->getStageName($this->data['stage']);
-        }
         return $done;
     }
 
@@ -133,19 +101,6 @@ class shopGeneratorPluginRunController extends waLongActionController
             'memory_avg' => sprintf('%0.2fMByte', $this->data['memory_avg'] / 1048576),
         );
 
-        $stage_num = 0;
-        $stage_count = count($this->data['current']);
-        foreach ($this->data['current'] as $stage => $current) {
-            if ($current < $this->data['count'][$stage]) {
-                $response['stage'] = $stage;
-                $response['progress'] = sprintf('%0.3f%%', 100.0 * (1.0 * $current / $this->data['count'][$stage] + $stage_num) / $stage_count);
-                break;
-            }
-            ++$stage_num;
-        }
-        $response['stage_name'] = $this->data['stage_name'];
-        $response['stage_num'] = $stage_num;
-        $response['stage_count'] = $stage_count;
         $response['current_count'] = $this->data['current'];
         $response['processed_count'] = $this->data['processed_count'];
         if ($response['ready']) {
@@ -159,15 +114,7 @@ class shopGeneratorPluginRunController extends waLongActionController
         $report = '<div class="successmsg">';
         $report .= sprintf('<i class="icon16 yes"></i>%s ', _wp('Successfully'));
         $chunks = array();
-        foreach ($this->data['current'] as $stage => $current) {
-            if ($current) {
-                if ($this->data['reader']) {
-                    if ($data = $this->getStageReport($stage, $this->data['processed_count'])) {
-                        $chunks[] = htmlentities($data, ENT_QUOTES, 'utf-8');
-                    }
-                }
-            }
-        }
+
         $report .= implode(', ', $chunks);
         if (!empty($this->data['timestamp'])) {
             $interval = time() - $this->data['timestamp'];
@@ -193,69 +140,9 @@ class shopGeneratorPluginRunController extends waLongActionController
         $result = false;
         if ($this->getRequest()->post('cleanup')) {
             $result = true;
-            if ($this->reader) {
-                $this->reader->finish();
-            } elseif ($this->data['reader']) {
-                $this->data['reader']->finish();
-            }
+
         }
         return $result;
-    }
-
-    /**
-     * @param string $id
-     * @param array $options
-     * @return shopGeneratorPluginReader
-     * @throws waException
-     */
-    private function getReader($id, $options = array())
-    {
-        $class = 'shopGeneratorPlugin' . ucfirst($id) . 'Reader';
-
-        if ($id && class_exists($class)) {
-            if (isset($options['reader'])) {
-                unset($options['reader']);
-            }
-
-            /** @var shopGeneratorPluginReader $reader */
-            $reader = new $class($options);
-        } else {
-            throw new waException('Reader not found');
-        }
-
-        if (!($reader instanceof shopGeneratorPluginReader)) {
-            throw new waException('Invalid reader');
-        }
-
-        return $reader;
-    }
-
-    /**
-     * @param string $stage_id
-     * @return string
-     */
-    private function getStageName($stage_id)
-    {
-        if($stage_id == 'clear_qty') {
-            return "Обнуляем остатки на складе...";
-        }
-
-        return $this->reader->getStageName($stage_id);
-    }
-
-    private function getStageReport($stage_id, $data)
-    {
-        if($stage_id == 'clear_qty') {
-            return "Обнулены остатки по всем товарам на складе.";
-        }
-
-        return $this->data['reader']->getStageReport($stage_id, $data);
-    }
-
-    protected function restore()
-    {
-        $this->reader = $this->data['reader'];
-        $this->reader->restore();
     }
 
 }

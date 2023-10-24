dce597b991bdbcf43f95e1c6f5db0471e1159b3d
Andrew Godin <covoxx@gmail.com>
2023-10-24 14:43:32 +0300

    1.6.0

--- lib/config/requirements.php
+++ lib/config/requirements.php
@@ -3,7 +3,7 @@
 return array(
     'app.shop' => array(
         'strict'  => TRUE,
-        'version' => '5.2.0'
+        'version' => '7.4.0'
     ),
     'php.bcmath' => array(
         'description' => _wp('This extension is used for computing'), 'strict' => true,
